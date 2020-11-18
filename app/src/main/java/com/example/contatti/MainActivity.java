e01992bc053c4f9860bf67ae4ae07c511d163c60
Giada <giadagabriele@outlook.com>
2020-11-18 16:05:40 +0100

    Log Out aggiunto

--- app/src/main/java/com/example/contatti/MainActivity.java
+++ app/src/main/java/com/example/contatti/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.contatti.ui.login.Login;
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,7 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
  public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         auth = FirebaseAuth.getInstance();
@@ -19,8 +22,19 @@ import com.google.firebase.auth.FirebaseAuth;
             startActivity(new Intent(getApplicationContext(), Login.class));
             finish();
         }
-        else{
+        else {
             setContentView(R.layout.activity_main);
+            final Button logout=findViewById(R.id.esci);
+            logout.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    auth.signOut();
+                    startActivity(new Intent(getApplicationContext(), Login.class));
+                    finish();
+                }
+            });
         }
     }
+
 }
